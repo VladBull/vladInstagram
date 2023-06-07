897f3dc82b1a0ba57e6e1a662b822392fb80b0a2
Vladimir Buliga <buligavladimir@yahoo.com>
2023-06-07 15:07:53 +0300

    2nd commit

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -52,7 +52,7 @@ class PostsController extends Controller
         $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
         $image->save();
 
-        //TODO: This error should be because of VSC. Test on PHStorm.
+        //TODO: This error should be because of VSC. Tested on PHStorm and it's ok.
         auth()->user()->posts()->create([
             'caption' => $data['caption'],
             'image' => $imagePath,
